8133f3aa6f2e4b89e6d7f31a24530eda9f991e52
Hector <hectormestre1234@gmail.com>
2024-10-17 03:33:35 -0500

    new stuff

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import "./App.css";  // Import global styles
+// src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './App.css';  // Make sure this path is correct
+import App from './App';  // Make sure this path is correct
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
