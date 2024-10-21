853b53fb15f0bc8da62578f9aa25618b7b4eb660
Hector <hectormestre1234@gmail.com>
2024-10-21 02:39:28 -0500

    done for today

--- src/components/ContentPage.js
+++ src/components/ContentPage.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import './contentpage.css'; // Import your custom CSS for styling
+
 
 const ContentPage = () => {
   const { id } = useParams(); // Get the ID from the URL
