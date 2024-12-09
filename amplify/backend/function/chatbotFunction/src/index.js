f40fe1baa46f5c51eec5d46efb82e1b1234d140f
Hector <hectormestre1234@gmail.com>
2024-12-09 12:33:44 -0600

    Few chatbot improvments in the UI

--- amplify/backend/function/chatbotFunction/src/index.js
+++ amplify/backend/function/chatbotFunction/src/index.js
@@ -28,7 +28,7 @@ exports.handler = async (event) => {
     };
 
     const requestBody = JSON.stringify({
-        model: 'ft:gpt-3.5-turbo-0125:followingsunnah::AcU1dthp',
+        model: 'ft:gpt-3.5-turbo-0125:followingsunnah::AccZPEz8',
         messages: [{ role: 'user', content: userMessage }],
         max_tokens: 200,
     });
